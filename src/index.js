3cc206c6cae1de9ffe3c40434f2801ea562b46b4
aglaya <aglayastunkus1@gmail.com>
2021-07-30 15:55:31 +0300

    feat: implement functions min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+exports.min = function min(array) {
+    return (arguments.length === 0 || array.length === 0) ? 0 : Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return (arguments.length === 0 || array.length === 0) ? 0 : Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return (arguments.length === 0 || array.length === 0) ? 0 : array.reduce((sum, i) => sum + i) / array.length;
+};
